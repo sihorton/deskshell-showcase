5429d7bb06c10829c90ee891c445916d7a7015c2
sihorton <sij.horton@gmail.com>
2013-11-09 22:23:29 +0100

    updated paths.

--- chrome-app/launchTest.bat
+++ chrome-app/launchTest.bat
@@ -1,3 +1,2 @@
-REM C:\Users\simon\AppData\Local\Deskshell\bin\win\DeskshellChrome\App\Chrome-bin\deskshell-chrome.exe --load-and-launch-app=%cd%
-D:\dev\GoogleChromePortable\App\Chrome-bin\chrome.exe --load-and-launch-app=%cd%
+C:\Users\simon\AppData\Local\Deskshell\bin\win\DeskshellChrome\App\Chrome-bin\deskshell-chrome.exe --load-and-launch-app=%cd%
 pause

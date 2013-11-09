b573fdcf3170cd89fd9049030361c61e80abce9b
sihorton <sij.horton@gmail.com>
2013-11-09 22:21:16 +0100

    Added example of running a chrome app, this works and we can even run without window chrome, but it has a big problem that a new chrome browser window is opened in addition to the app (http://crbug.com/175381). Once the chrome bug is fixed then we can launch chrome apps from deskshell.

---
+++ chrome-app/launchTest.bat
@@ -0,0 +1,3 @@
+REM C:\Users\simon\AppData\Local\Deskshell\bin\win\DeskshellChrome\App\Chrome-bin\deskshell-chrome.exe --load-and-launch-app=%cd%
+D:\dev\GoogleChromePortable\App\Chrome-bin\chrome.exe --load-and-launch-app=%cd%
+pause
